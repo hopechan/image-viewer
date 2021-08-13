76705bf33fd1c63e3e58b1c191e3834f93ed446c
Esperanza Dueñas <da13002@ues.edu.sv>
2021-08-12 19:16:23 -0600

    feat(viewer): add method to consume api

--- src/app/viewer/viewer.component.ts
+++ src/app/viewer/viewer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Earth, EarthService } from '../services/earth.service';
 
 @Component({
   selector: 'app-viewer',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./viewer.component.css']
 })
 export class ViewerComponent implements OnInit {
+  earth!: Earth;
 
-  constructor() { }
+  constructor(private earthService: EarthService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getData();
+  }
+
+  getData(){
+    this.earthService.getData({lon:"-95.33", lat: "29.78", date: "2018-01-01", dim: "0.10"}).subscribe((data: Earth) => {
+      this.earth = data;
+    });
   }
 
 }
