68b8fb3c2879a7604b7de70da055f905ffbcc943
Esperanza Dueñas <da13002@ues.edu.sv>
2021-08-12 19:17:45 -0600

    feat(app.module): add HttpClient and provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ViewerComponent } from './viewer/viewer.component';
+import { EarthService } from './services/earth.service';
+import { EarthFormComponent } from './earth-form/earth-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ViewerComponent
+    ViewerComponent,
+    EarthFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    EarthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
