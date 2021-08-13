02d4b72fe8c22caf6e31262f9875ae5e5b80c63b
Esperanza Dueñas <da13002@ues.edu.sv>
2021-08-12 20:46:48 -0600

    refactor(app-rounting): change path for components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EarthFormComponent } from './earth-form/earth-form.component';
 
 import { ViewerComponent } from "./viewer/viewer.component";
 
 const routes: Routes = [
-  { path: "", component: ViewerComponent }
+  {path: "", component: EarthFormComponent},
+  { path: "result", component: ViewerComponent }
 ];
 
 @NgModule({
